4a799df5c28e1002ee4d2362ddc3719228f06c7a
teemocaption <eyan0816@gmail.com>
2024-05-12 02:40:46 +0800

    暫存

--- frontend/src/components/stockpages/PredictStock.vue
+++ frontend/src/components/stockpages/PredictStock.vue
@@ -18,27 +18,14 @@ export default {
     },
     created() {
         this.stockCode = this.$route.params.stockcode;
-        this.updateDimensions();
     },
     mounted() {
-        window.addEventListener('resize', this.updateDimensions);
         this.fetchStockData();
     },
     beforeUnmount() {
         window.removeEventListener('resize', this.updateDimensions);
     },
     methods: {
-        updateDimensions() {
-            this.$nextTick(() => {
-                const container = this.$refs.chart;
-                if (!container) return;
-
-                this.width = container.clientWidth;
-                this.height = container.clientHeight;
-
-                this.drawChart();
-            });
-        },
         fetchStockData() {
             axios.get(`/stock/${this.stockCode}/getpredict`, {})
                 .then(response => {
@@ -58,57 +45,7 @@ export default {
                     console.error('Error fetching stock data:', error);
                 });
         },
-        drawChart() {
-            const container = this.$refs.chart;
-            if (!container || !this.stockData.length) return;
-
-            d3.select(container).select('svg').remove();
-
-            const svg = d3.select(container)
-                .append('svg')
-                .attr('width', '100%')
-                .attr('height', '100%')
-                .style('background-color', 'white');
-
-            const margin = { top: 20, right: 70, bottom: 60, left: 80 };
-            const drawingWidth = this.width - margin.left - margin.right;
-            const drawingHeight = this.height - margin.top - margin.bottom;
-
-            const g = svg.append('g').attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-            const x = d3.scaleTime()
-                .domain([d3.min(this.stockData, d => new Date(d.date)), d3.max(this.stockData, d => new Date(d.date))])
-                .range([0, drawingWidth]);
-
-            const yPrice = d3.scaleLinear()
-                .domain([0, d3.max(this.stockData, d => d.close)])
-                .range([drawingHeight, 0]);
-
-            const line = d3.line()
-                .x(d => x(new Date(d.date)))
-                .y(d => yPrice(d.close));
-
-            g.append('path')
-                .datum(this.stockData)
-                .attr('fill', 'none')
-                .attr('stroke', '#be0027')
-                .attr('stroke-width', 2)
-                .attr('d', line);
-
-            this.stockData.forEach(data => {
-                g.append('circle')
-                    .attr('cx', x(new Date(data.date)))
-                    .attr('cy', yPrice(data.close))
-                    .attr('r', 5)
-                    .attr('fill', '#be0027');
-            });
-
-            g.append('g')
-                .attr('transform', `translate(0, ${drawingHeight})`)
-                .call(d3.axisBottom(x).ticks(d3.timeDay.every(1)).tickFormat(d3.timeFormat('%m/%d')));
-
-            g.append('g').call(d3.axisLeft(yPrice));
-        }
+        
     }
 }
 </script>
